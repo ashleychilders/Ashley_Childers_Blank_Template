5370204d05c10bfdb791ff6e2e1b79ac09915263
Ashley Childers <ashley.childers@gmail.com>
2016-08-29 18:30:40 -0400

    update index file

--- Ashley_Childers_Blank_Template/index.php
+++ Ashley_Childers_Blank_Template/index.php
@@ -1,10 +1,39 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-<title>Ashley's WordPress Template</title>
+  <meta charset="utf-8">
+  <title>Ashley Childers Blank Template</title>
+
+  <link rel="stylesheet" type="text/css" href="
+    <?php bloginfo('stylesheet_url'); ?>"/>
+
+  <!--
+  <link rel="stylesheet" type="text/css" href="style.css" />
+
+  <script src-"custom.js"></script>
+-->
 </head>
 <body>
-  <h1>Blank WordPress Template</h1>
-  <p>This is my WordPress template.</p>
+  <div class="container">
+    <header class="row">
+      <div class="twelve columns">
+          <h1>Ashley Childers Blank WordPress Template</h1>
+          <p>This is my WordPress template.</p>
+      </div>
+    </header>
+    <section class ="row">
+      <div class="twelve columns">
+          <h2>Section Content - cool</h2>
+          <p>This is a cool section</p>
+      </div>
+    </section>
+    <footer class="row">
+      <div class="twelve columns">
+          <h2>This is the Footer</h2>
+          <p>This is some cool footer content</p>
+      </div>
+  </footer>
+</div>
+
 </body>
 </html>
