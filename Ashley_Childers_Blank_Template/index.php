7e3f197906519c236fdbb48fb766dc7dacee2acb
Ashley Childers <ashley.childers@gmail.com>
2016-09-11 20:22:52 -0400

    Link to header and footer

--- Ashley_Childers_Blank_Template/index.php
+++ Ashley_Childers_Blank_Template/index.php
@@ -1,6 +1,8 @@
+<?php get_header(); ?>
 <section class ="row">
   <div class="twelve columns">
       <h2>Section Content - cool</h2>
       <p>This is a cool section</p>
   </div>
 </section>
+<?php get_footer(); ?>
