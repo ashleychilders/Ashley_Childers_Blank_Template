fe1e4de4173067eaddfe0653501befc41c650ec7
Ashley Childers <ashley.childers@gmail.com>
2016-09-11 20:17:20 -0400

    Add text to header file

---
+++ Ashley_Childers_Blank_Template/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+        <!-- Links to our Style.css file -->
+        <link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>" />
+    </head>
+    <body>
+        <div class="container">
+            <header class="row">
+                <div class="twelve columns">
+                    <h1>Blank WordPress Template</h1>
+                    <h2>This is my WordPress template.</h2>
+                </div>
+            </header>
+<!-- END OF HEADER - BEGIN SECTION CONTENT -->
