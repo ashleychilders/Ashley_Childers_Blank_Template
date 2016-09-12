b84e0c22cc3306f96a0c56890a6ba7cb6d541858
Ashley Childers <ashley.childers@gmail.com>
2016-09-11 20:21:41 -0400

    Update footer

---
+++ Ashley_Childers_Blank_Template/footer.php
@@ -0,0 +1,9 @@
+<!-- BEGINNING OF FOOTER - END SECTION CONTENT -->
+            <footer class="row">
+                <div class="twelve columns">
+                    <p>Footer content goes here.</p>
+                </div>
+            </footer>
+        </div> <!-- ends container -->
+    </body>
+</html>
