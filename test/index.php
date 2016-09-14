cb0eaee93ee6cfc0dcbeb8a5a640a4b6c9f27f4b
Ashley Childers <ashley.childers@gmail.com>
2016-09-13 22:31:19 -0400

    testing out files

---
+++ test/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Ashley Childers Blank Template</title>
+</head>
+<body>
+  <h1>Blank WordPress Template</h1>
+  <p>This is my WordPress template.</p>
+</body>
+</html>
