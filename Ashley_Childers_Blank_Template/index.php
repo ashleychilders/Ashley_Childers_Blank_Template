6c38e2cd9d529177030309f274d53d3229f61e58
Ashley Childers <ashley.childers@gmail.com>
2016-09-13 23:26:04 -0400

    Update files to try to fix wp

--- Ashley_Childers_Blank_Template/index.php
+++ Ashley_Childers_Blank_Template/index.php
@@ -2,7 +2,6 @@
 <html lang="en">
 <head>
   <title>Ashley Childers Blank Template</title>
-  <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
   <h1>Ashley's Blank WordPress Template</h1>
