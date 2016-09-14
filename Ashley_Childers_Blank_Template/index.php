987faf3f3c20382a949ae02be0fca3b49f5be177
Ashley Childers <ashley.childers@gmail.com>
2016-09-13 23:19:08 -0400

    Update css

--- Ashley_Childers_Blank_Template/index.php
+++ Ashley_Childers_Blank_Template/index.php
@@ -2,6 +2,7 @@
 <html lang="en">
 <head>
   <title>Ashley Childers Blank Template</title>
+  <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
   <h1>Ashley's Blank WordPress Template</h1>
