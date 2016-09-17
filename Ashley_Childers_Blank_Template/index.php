2b87b44cbeea5e383570bd2ff3ff8376835b3b14
Ashley Childers <ashley.childers@gmail.com>
2016-09-17 14:30:27 -0400

    Update to index file

--- Ashley_Childers_Blank_Template/index.php
+++ Ashley_Childers_Blank_Template/index.php
@@ -1,8 +1,17 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
+  <meta charset="utf-8">
   <title>Ashley Childers Blank Template</title>
-</head>
+
+  <link rel="stylesheet" type="text/css" href="
+    <?php bloginfo('stylesheet_url'); ?>"/>
+
+  <!--
+  <link rel="stylesheet" type="text/css" href="style.css" />
+
+  <script src-"custom.js"></script>
+-->
 <body>
   <h1>Ashley's Blank WordPress Template</h1>
   <p>This is my WordPress template.</p>
