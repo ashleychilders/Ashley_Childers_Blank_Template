399af63d0e148b39133b635907065b7c6bab3a08
Ashley Childers <ashley.childers@gmail.com>
2016-09-19 19:52:33 -0400

    Remove text from index file and add to header footer files

--- Ashley_Childers_Blank_Template/index.php
+++ Ashley_Childers_Blank_Template/index.php
@@ -14,24 +14,30 @@
 
 <body>
   <div class="container">
-    <header class="row">
-        <div class="twelve columns">
-            <h1>Ashley Childers Blank WordPress Template</h1>
-            <p>This is my WordPress template.</p>
-        </div>
+  <!-- Begin Header -->
+      <header class="row">
+          <div class="twelve columns">
+              <h1>Ashley Childers Blank WordPress Template</h1>
+              <p>This is my WordPress template.</p>
+            </div>
       </header>
+  <!-- End Header -->
+  <!-- Begin Section Container -->
       <section class ="row">
-        <div class="twelve columns">
-            <h2>Section Content - cool</h2>
-            <p>This is a cool section</p>
-        </div>
+          <div class="twelve columns">
+              <h2>Section Content - cool</h2>
+              <p>This is a cool section</p>
+          </div>
       </section>
+  <!-- End Section Container -->
+  <!-- Begin Fooder -->
       <footer class="row">
-        <div class="twelve columns">
-            <h2>This is the Footer</h2>
-            <p>This is some cool footer content</p>
-        </div>
+          <div class="twelve columns">
+              <h2>This is the Footer</h2>
+              <p>This is some cool footer content</p>
+          </div>
   </footer>
+<!-- End Footer -->
 </div>
 </body>
 </html>
