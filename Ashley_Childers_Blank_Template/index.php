ff9ab28706cc169b6d4b3d4e98676ac54543df91
Ashley Childers <ashley.childers@gmail.com>
2016-09-21 19:57:08 -0400

    Edit to php file

--- Ashley_Childers_Blank_Template/index.php
+++ Ashley_Childers_Blank_Template/index.php
@@ -24,7 +24,7 @@
             <?php
             if ( have_posts() ) {
               while ( have_posts() ){
-                the_post();
+                the_post(); ?>
 
                 <h3><?php the_title(); ?></h3>
                 <?php the_excerpt(); ?>
