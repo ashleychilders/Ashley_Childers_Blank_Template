8e6204e960ff75d74c440a7f5d327a6d159e0eec
Ashley Childers <ashley.childers@gmail.com>
2016-09-24 13:19:05 -0400

    Add footer widgets

--- Ashley_Childers_Blank_Template/functions.php
+++ Ashley_Childers_Blank_Template/functions.php
@@ -11,6 +11,37 @@ function blank_widgets_init() {
         'before_title' => '<h2>',
         'after_title' => '</h2>'
         ));
+/*-------------- First Footer Widget --------------- */
+    register_sidebar( array(
+        'name'          => ('First Footer Widget'),
+        'id'            => 'footer-one',
+        'description'   => 'Left widget in the footer',
+        'before_widget' => '<div class="widget-footer widget-left">',
+        'after_widget'  => '</div>',
+        'before_title'  => '<h3>',
+        'after_title'   => '</h3>'
+        ));
+/*-------------- Second Footer Widget --------------- */
+register_sidebar( array(
+    'name'          => ('Second Footer Widget'),
+    'id'            => 'footer-two',
+    'description'   => 'Middle widget in the footer',
+    'before_widget' => '<div class="widget-footer widget-middle">',
+    'after_widget'  => '</div>',
+    'before_title'  => '<h3>',
+    'after_title'   => '</h3>'
+    ));
+/*-------------- Third Footer Widget -------------- */
+register_sidebar( array(
+    'name'          => ('Third Footer Widget'),
+    'id'            => 'footer-three',
+    'description'   => 'Right widget in the footer',
+    'before_widget' => '<div class="widget-footer widget-right">',
+    'after_widget'  => '</div>',
+    'before_title'  => '<h3>',
+    'after_title'   => '</h3>'
+    ));
+
     }
 add_action('widgets_init', 'blank_widgets_init');
 ?>
