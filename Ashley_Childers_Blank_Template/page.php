d5a6decc11cc55bb01de9c26ee37c3f15e033052
Ashley Childers <ashley.childers@gmail.com>
2016-09-24 12:46:09 -0400

    Correct code on page file

--- Ashley_Childers_Blank_Template/page.php
+++ Ashley_Childers_Blank_Template/page.php
@@ -4,21 +4,19 @@
       <div class="nine columns">
 
 <!-- BEGIN PAGE PHP -->
-            <?php
-                if( have_posts() ){
-                    while( have_posts() ){
-                      the_post(); ?>
-
-                  <h2><?php the_title(); ?></h2>
-                  <?php the_content();
-                endwhile;
-              endif; ?>
+          <?php if (have_posts()) :
+            /* OUR DATA CONTEXT IS DEFINED  */
+            while (have_posts()) : the_post(); ?>
+                <h2><?php the_title(); ?></h2>
+                <?php the_content();
+            endwhile;
+          endif; ?>
       </div>
 
       <div class="three columns">
           <?php get_sidebar(); ?>
       </div>
-      
+
     </div>
 
 <!-- End Section Container -->
