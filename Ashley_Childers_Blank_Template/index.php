04c068ea8772b951d08d786fe23d47b06080837e
Ashley Childers <ashley.childers@gmail.com>
2016-09-24 18:21:27 -0400

    Update to single page for thumbnail

--- Ashley_Childers_Blank_Template/index.php
+++ Ashley_Childers_Blank_Template/index.php
@@ -29,7 +29,7 @@
                   if ( has_post_thumbnail() ) {
                     the_post_thumbnail('thumbnail');
                   }
-                ?>        
+                ?>
                 <h3><?php the_title(); ?></h3>
                 <?php the_excerpt(); ?>
 
