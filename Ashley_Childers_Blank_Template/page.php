c398102acb5137972f07abf1b7836c510dba4cb3
Ashley Childers <ashley.childers@gmail.com>
2016-09-25 14:04:55 -0400

    Edit layout for ease of reading

--- Ashley_Childers_Blank_Template/page.php
+++ Ashley_Childers_Blank_Template/page.php
@@ -6,10 +6,10 @@
 <!-- BEGIN PAGE PHP -->
           <?php if (have_posts()) :
             /* OUR DATA CONTEXT IS DEFINED  */
-            while (have_posts()) : the_post(); ?>
-                <h2><?php the_title(); ?></h2>
-                <?php the_content();
-            endwhile;
+              while (have_posts()) : the_post(); ?>
+                    <h2><?php the_title(); ?></h2>
+                    <?php the_content();
+              endwhile;
           endif; ?>
       </div>
 
